4866e87421734954778b838b53eade9d1984e8a6
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-09-02 09:42:26 -0300

    estudo gradle, springData

---
+++ src/main/java/com/jornada/mentoria/mentoriaapi/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.jornada.mentoria.mentoriaapi.exceptions;
+
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
